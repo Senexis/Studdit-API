adac6ae9589ea42545f089bf53af72af6bf6c42a
Senexys <timoviveen@live.nl>
2018-11-26 10:30:54 +0100

    Let me click the links in the terminal.

--- app.js
+++ app.js
@@ -16,14 +16,14 @@ var conn = mongoose.connection;
 conn.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 conn.once('open', () => {
-	console.log('connected to database')
+	console.log('MongoDB connected.')
 });
 
 app.listen(process.env.PORT || 5000, () => {
 	if (process.env.PORT !== undefined) {
-		console.log('Server gestart op poort ' + process.env.PORT);
+		console.log($`Server started at "http://localhost:${process.env.PORT}/".`);
 	} else {
-		console.log('Server gestart op poort 5000');
+		console.log($`Server started at "http://localhost:5000/".`);
 	}
 });
 

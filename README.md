3a6a27432a04cb549536d6fd488156d0ce597cf3
Wouter Jansen <wouter97@planet.nl>
2018-11-26 09:18:38 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+"# NoSQLBois" 
+
+# Online forum 
---
+++ test/create_test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('Testing if app runs', () => {
+    it('Running..', (done) => {
+        done();
+    })
+});

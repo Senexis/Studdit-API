1a1e9ad932ff1e586edbb459ea245d8e8e8bf120
Senexys <timoviveen@live.nl>
2018-11-26 10:32:01 +0100

    This isn't C#. #BrainFart

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ conn.once('open', () => {
 
 app.listen(process.env.PORT || 5000, () => {
 	if (process.env.PORT !== undefined) {
-		console.log($`Server started at "http://localhost:${process.env.PORT}/".`);
+		console.log(`Server started at "http://localhost:${process.env.PORT}/".`);
 	} else {
-		console.log($`Server started at "http://localhost:5000/".`);
+		console.log(`Server started at "http://localhost:5000/".`);
 	}
 });
 

82ee555447b9135bc78bdb8650b729e9e5cb62c2
Senexys <timoviveen@live.nl>
2018-11-26 11:06:27 +0100

    WIP: Threads controller and routes.

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,16 @@
+
+const ThreadsController = require('../controllers/threads_controller');
+
 module.exports = (app) => {
-    app.get('*', (req, res) => {
-        res.status(200).send({
-            message: 'Application is running'
-        }).end();
-    })
+    // app.get('*', (req, res) => {
+    //     res.status(200).send({
+    //         message: 'Application is running'
+    //     }).end();
+    // })
+
+    // Thread endpoints
+    app.post('/api/threads', ThreadsController.create);
+    app.put('/api/threads/:id', ThreadsController.edit);
+    app.delete('/api/threads/:id', ThreadsController.delete);
+    app.get('/api/threads', ThreadsController.index);
 };

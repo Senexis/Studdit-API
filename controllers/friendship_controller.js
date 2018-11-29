3068a3b6c81710b829b49d1bce92a2b8df2cccc9
Wouter Jansen <wouter97@planet.nl>
2018-11-29 12:10:13 +0100

    Added username checking when deleting a Friend

--- controllers/friendship_controller.js
+++ controllers/friendship_controller.js
@@ -9,19 +9,15 @@ module.exports = {
             username: req.body.username,
             friendName: req.body.friendName
         }
-        console.log(params)
         if (params.username === undefined || params.friendName === undefined) {
             res.status(409).json("Please enter your username and a friend's name");
         }
-        //TODO: Clean this up
         session.run('MATCH (u:user {username: $username}), (f:user {username: $friendName}) return u', params)
             .then((result) => {
-
                 if (!result.records[0]) {
                     console.log("username or friend does not exist");
                     res.status(409).json("username does not exist");
                 } else {
-                    //A relationship always has a direction. We can create a relationship both ways however.
                     session.run('MATCH (u:user {username: $username}), (f:user {username: $friendName}) MERGE (u)-[:Friends]-(f)', params)
                         .then((result) => {
                             res.status(201).json("Relationship successfully created!");
@@ -40,10 +36,17 @@ module.exports = {
         if (params.username === undefined || params.friendName === undefined) {
             res.status(409).json("Please enter your username and a friend's name");
         }
-        //TODO: catch typo or wrong name
-        session.run('MATCH (u:user {username: $username})-[r:Friendship]-(:user {username: $friendName}) DELETE r', params)
-            .then(() => {
-                res.status(201).json("Relationship removed")
-            }).catch(next);
-    }
+        session.run('MATCH (u:user {username: $username}), (f:user {username: $friendName}) return u', params)
+            .then((result) => {
+                if (!result.records[0]) {
+                    console.log("username or friend does not exist");
+                    res.status(409).json("username does not exist");
+                } else {
+                    session.run('MATCH (u:user {username: $username})-[r:Friends]-(:user {username: $friendName}) DELETE r', params)
+                    .then(() => {
+                        res.status(201).json("Relationship removed")
+                    }).catch(next);
+                }
+            }).catch(next)
+        }
 };

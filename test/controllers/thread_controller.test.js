bbaa05cca3737889f74f364807c3c5af81d8c5e0
Senexis <timoviveen@live.nl>
2018-11-30 12:28:38 +0100

    WIP: Create test template for GET requests.

---
+++ test/controllers/thread_controller.test.js
@@ -0,0 +1,49 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const expect = chai.expect();
+const {
+    session,
+    neo4j
+} = require('../../neodb');
+const app = require('../../app');
+
+const Thread = require('../../models/thread');
+
+chai.use(chaiHttp);
+
+describe('Assessment of thread requirements', () => {
+    it('should get all threads using /api/threads/', () => {
+        chai.request(app)
+            .get('/api/threads/')
+            .end((err, res) => {
+                res.should.have.status(201);
+
+                // TODO.
+
+                done();
+            });
+    });
+    it('should get a thread using /api/threads/:id/', () => {
+        const NewThread = {
+            username: "Test1",
+            title: "Test1 Title",
+            content: "Test1 Content"
+        }
+
+        Thread.create(NewThread)
+            .then(thread => {
+                chai.request(app)
+                    .get(`/api/threads/${thread.id}/`)
+                    .end((err, res) => {
+                        res.should.have.status(201);
+        
+                        // TODO.
+        
+                        done();
+                    });
+            })
+            .catch(next)
+
+    });
+});
--- test/controllers/user_controller_test.js
+++ test/controllers/user_controller_test.js
@@ -5,8 +5,8 @@ const expect = chai.expect();
 const {
     session,
     neo4j
-} = require('../neodb');
-const app = require('../app');
+} = require('../../neodb');
+const app = require('../../app');
 
 let name = "Hect0r";
 let pwd = "secretPassword";
@@ -14,7 +14,7 @@ let newPwd = "notSoSecretPassword";
 
 chai.use(chaiHttp);
 
-describe("Assesment of all user requirements", () => {
+describe("Assessment of user requirements", () => {
     // it("Creation of user requires username and password", (done) => {
     //     //Posting without contents gives an error
     //     chai.request(app).post('/api/users')
@@ -28,62 +28,78 @@ describe("Assesment of all user requirements", () => {
         chai.request(app)
             .post('/api/users')
             .set('content-type', 'application/x-www-form-urlencoded')
-            .send({username: name, password: pwd})
+            .send({
+                username: name,
+                password: pwd
+            })
             .end((err, res) => {
-                  res.should.have.status(201);
-              done();
-        });
+                res.should.have.status(201);
+                done();
+            });
     });
     it("Creation of user requires unique username", (done) => {
-            chai.request(app)
-                .post('/api/users')
-                .set('content-type', 'application/x-www-form-urlencoded')
-                .send({username: name, password: pwd})
-                .end((err, res) => {
-                      res.should.have.status(401);
-                  done();
+        chai.request(app)
+            .post('/api/users')
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .send({
+                username: name,
+                password: pwd
+            })
+            .end((err, res) => {
+                res.should.have.status(401);
+                done();
             });
     });
     it("Update of password requires a correct current password", (done) => {
         chai.request(app)
             .put('/api/users/' + name)
             .set('content-type', 'application/x-www-form-urlencoded')
-            .send({password: pwd, newPassword: newPwd})
+            .send({
+                password: pwd,
+                newPassword: newPwd
+            })
             .end((err, res) => {
                 res.should.have.status(201);
                 pwd = newPwd;
-            done();
-        });
+                done();
+            });
     });
     it("Update that has error returns 401", (done) => {
         chai.request(app)
             .put('/api/users/' + name)
             .set('content-type', 'application/x-www-form-urlencoded')
-            .send({password: 'totallyWrongPassword', newPassword: newPwd})
+            .send({
+                password: 'totallyWrongPassword',
+                newPassword: newPwd
+            })
             .end((err, res) => {
                 res.should.have.status(401);
-            done();
-        });
+                done();
+            });
     });
     it("Delete that has error returns 401", (done) => {
         chai.request(app)
             .delete('/api/users/' + name)
             .set('content-type', 'application/x-www-form-urlencoded')
-            .send({password: 'totallyWrongPassword'})
+            .send({
+                password: 'totallyWrongPassword'
+            })
             .end((err, res) => {
-                  res.should.have.status(401);
-              done();
-        });
+                res.should.have.status(401);
+                done();
+            });
     });
     it("Delete that properly works when username and password are provided", (done) => {
         chai.request(app)
             .delete('/api/users/' + name)
             .set('content-type', 'application/x-www-form-urlencoded')
-            .send({password: pwd})
+            .send({
+                password: pwd
+            })
             .end((err, res) => {
-                  res.should.have.status(204);
-              done();
-        });
+                res.should.have.status(204);
+                done();
+            });
     });
     // it("Threads and comments of removed user still exist", (done) => {
     //     done();
@@ -96,40 +112,52 @@ describe("Assesment of all friendship requirements", () => {
         chai.request(app)
             .post('/api/friendships')
             .set('content-type', 'application/x-www-form-urlencoded')
-            .send({username: 'Harry', friendName: 'Peter'})
+            .send({
+                username: 'Harry',
+                friendName: 'Peter'
+            })
             .end((err, res) => {
-                  res.should.have.status(201);
-              done();
-        });
+                res.should.have.status(201);
+                done();
+            });
     });
     it("Adding the same friendship does not cause an error", (done) => {
         chai.request(app)
             .post('/api/friendships')
             .set('content-type', 'application/x-www-form-urlencoded')
-            .send({username: 'Harry', friendName: 'Peter'})
+            .send({
+                username: 'Harry',
+                friendName: 'Peter'
+            })
             .end((err, res) => {
-                  res.should.have.status(201);
-              done();
-        });
+                res.should.have.status(201);
+                done();
+            });
     });
     it("Removal of a friendship requires two correct usernames", (done) => {
         chai.request(app)
             .delete('/api/friendships')
             .set('content-type', 'application/x-www-form-urlencoded')
-            .send({username: 'Harry', friendName: 'Peter'})
+            .send({
+                username: 'Harry',
+                friendName: 'Peter'
+            })
             .end((err, res) => {
-                  res.should.have.status(201);
-              done();
-        });
+                res.should.have.status(201);
+                done();
+            });
     });
     it("Removal of non-existing friendship does not cause an error", (done) => {
         chai.request(app)
             .delete('/api/friendships')
             .set('content-type', 'application/x-www-form-urlencoded')
-            .send({username: 'Harry', friendName: 'Peter'})
+            .send({
+                username: 'Harry',
+                friendName: 'Peter'
+            })
             .end((err, res) => {
-                  res.should.have.status(201);
-              done();
-        });
+                res.should.have.status(201);
+                done();
+            });
     });
 });

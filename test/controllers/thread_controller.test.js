84d5802160c48a9b561f09b43a18e8512e1a3dc0
Wouter Jansen <wouter97@planet.nl>
2019-02-04 08:54:43 +0100

    Added test for get assesment

--- test/controllers/thread_controller.test.js
+++ test/controllers/thread_controller.test.js
@@ -13,14 +13,24 @@ const Thread = require('../../models/thread');
 chai.use(chaiHttp);
 
 describe('Assessment of thread requirements', () => {
-    it('should get all threads using /api/threads/', () => {
+    it('should get all threads using /api/threads/', (done) => {
         chai.request(app)
             .get('/api/threads/')
             .end((err, res) => {
-                res.should.have.status(201);
-                // TODO.
+                if(err) {
+                    console.log(err);
+                    done(err)
+                } else {
+                    //We assume that items have been added; perhaps add these in the test
+                    res.should.have.status(200);
+                    //Check if body is not empty
+                    should.exist(res.body); //Check for null & not defined
+                    //Check if body contains more than one item (to check if multiple threads can be obtained)
+                    expect(res.body).to.have.length.above(1)
+                    console.log(res.body)
+                    done();
 
-                done();
+                }
             });
     });
     it('should get a thread using /api/threads/:id/', () => {
@@ -35,6 +45,7 @@ describe('Assessment of thread requirements', () => {
                 chai.request(app)
                     .get(`/api/threads/${thread.id}/`)
                     .end((err, res) => {
+                        //console.log(res)
                         res.should.have.status(201);
         
                         // TODO.
